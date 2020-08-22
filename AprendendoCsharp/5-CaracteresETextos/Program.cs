07362128cf94c350e45cec3d362c15c497fe636a
Gabriel Machado <gabriel.machado@wisereducacao.com>
2020-08-22 19:29:14 -0300

    Termino da aula 04-05 (22-08)

---
+++ AprendendoCsharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5");
+
+            //Character
+            char primeiraLetra = 'G';
+            Console.WriteLine("Primeira Letra:" + primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de tecnologia";
+            string cursosProgramacao = @"- .NET
+- Java
+- Javascript";
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+            Console.ReadLine();
+        }
+    }
+}
