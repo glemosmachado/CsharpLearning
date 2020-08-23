863a0176ba71d389882ef3e0b58f875cb2960b70
Gabriel Machado <gabriel.machado@wisereducacao.com>
2020-08-23 13:20:19 -0300

    Termino da Aula 06 (23-08)

---
+++ AprendendoCsharp/ImparPar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImparPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciação da variável inicializadora
+            int numero = 0;
+
+            //Enquanto o numero for menor ou igual a 100
+            while (numero <= 100)
+            {
+                //Se o mod do número for igual a zero, printa que é par 
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine(numero + " O número é par");
+                    numero++;
+                }
+                //Se o mod do número for diferente a zero, printa que é impar
+                else
+                {
+                    Console.WriteLine(numero + " O número é impar");
+                    numero++;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
