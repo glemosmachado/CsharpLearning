bb7de35e6f2501aa3783f65a0e3649d10ff28c77
Gabriel Machado <gabriel.machado@wisereducacao.com>
2020-08-24 21:31:08 -0300

    C# parte 2 - Termino da aula 01-02 (24-08)

---
+++ ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoGabriel = new ContaCorrente();
+            contaDoGabriel.titular = "Gabriel";
+            contaDoGabriel.agencia = 863;
+            contaDoGabriel.numero = 863452;
+            contaDoGabriel.saldo = 100;
+
+            Console.WriteLine("Nome: " + contaDoGabriel.titular);
+            Console.WriteLine("Agencia: " + contaDoGabriel.agencia);
+            Console.WriteLine("Numero: " + contaDoGabriel.numero);
+            Console.WriteLine("Saldo: " + contaDoGabriel.saldo);
+
+            contaDoGabriel.saldo += 200;
+            Console.WriteLine("Saldo: " + contaDoGabriel.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipo de Referência
+            ContaCorrente contaDoGabriel = new ContaCorrente();
+            //Variável de Referência
+            contaDoGabriel.titular = "Gabriel";
+            contaDoGabriel.agencia = 863;
+            contaDoGabriel.numero = 863452;
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência: "+ (contaDoGabriel == contaDaGabriela));
+
+            int idade = 23;
+            int idadeMaisUmaVez = 23;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (idade == idadeMaisUmaVez));
+
+            contaDoGabriel = contaDaGabriela;
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaDoGabriel == contaDaGabriela));
+
+            contaDaGabriela.saldo = 300;
+            Console.WriteLine(contaDaGabriela.saldo);
+            Console.WriteLine(contaDoGabriel.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
