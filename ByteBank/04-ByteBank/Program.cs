3b6de12224f796f83e65f2f505d964d04b05fbd7
Gabriel Machado <gabriel.machado@wisereducacao.com>
2020-09-13 21:04:07 -0300

    C# parte 2 - Termino da aula 04 (13-09)

--- ByteBank/04-ByteBank/Program.cs
+++ ByteBank/04-ByteBank/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _04_ByteBank
+namespace _05_ByteBank
 {
     class Program
     {
---
+++ ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta = new ContaCorrente();
+            //conta.titular = new Cliente();
+            //conta.titular.nome = "Gabriela Costa";
+            //conta.titular.profissao = "Desenvolvedora C#";
+            //conta.titular.cpf = "434.532.878-20";
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634257;
+
+            if (conta.titular == null) 
+            {
+                Console.WriteLine("A Referência é nula");
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine(conta.titular.nome);
+
+
+            Console.ReadLine();
+        }
+    }
+}
