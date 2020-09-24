3fa6ed7939660073db15c719b2ede272b9ff7076
Gabriel Machado <gabriel.machado@wisereducacao.com>
2020-09-23 22:39:07 -0300

    C# parte 3 - Termino da aula  01-02 (23-09)

---
+++ NewByteBank/ByteBank/Program.cs
@@ -0,0 +1,42 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario funcionario = new Funcionario();
+            funcionario.Nome = "Carlos";
+            funcionario.CPF = "546.879.125-20";
+            funcionario.Salario = 2000;
+
+            gerenciador.Registrar(funcionario);
+
+            Console.WriteLine(funcionario.Nome);
+            Console.WriteLine(funcionario.GetBonificacao());
+            
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "994.221.334-28";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonificação" +gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
